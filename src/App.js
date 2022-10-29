d7aca35d3ead8b0a6557a5b4ef1214a8778e1224
Zihan Zhang <hblues419@gmail.com>
2022-10-30 00:04:22 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+function App () {
+  return (
+    <>
+      <h1>iMovie React Application</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/api/tmdbApi.js
@@ -0,0 +1,90 @@
+const baseUrl = 'https://api.themoviedb.org/3/';
+
+export const getMovies = () => {
+  return fetch(
+    baseUrl + `discover/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&include_video=false&page=1`
+  ).then((response) => {
+    if (!response.ok) {
+      throw new Error(response.json().message);
+    }
+    return response.json();
+  })
+    .catch((error) => {
+      throw error;
+    });
+};
+
+export const getMovie = (args) => {
+  // console.log(args)
+  const [, idPart] = args.queryKey;
+  const { id } = idPart;
+  return fetch(
+    baseUrl + `movie/${id}?api_key=${process.env.REACT_APP_TMDB_KEY}`
+  ).then((response) => {
+    if (!response.ok) {
+      throw new Error(response.json().message);
+    }
+    return response.json();
+  })
+    .catch((error) => {
+      throw error;
+    });
+};
+
+export const getGenres = async () => {
+  return fetch(
+    baseUrl + "genre/movie/list?api_key=" +
+    process.env.REACT_APP_TMDB_KEY +
+    "&language=en-US"
+  ).then((response) => {
+    if (!response.ok) {
+      throw new Error(response.json().message);
+    }
+    return response.json();
+  })
+    .catch((error) => {
+      throw error;
+    });
+};
+
+export const getMovieImages = ({ queryKey }) => {
+  const [, idPart] = queryKey;
+  const { id } = idPart;
+  return fetch(
+    baseUrl + `movie/${id}/images?api_key=${process.env.REACT_APP_TMDB_KEY}`
+  ).then((response) => {
+    if (!response.ok) {
+      throw new Error(response.json().message);
+    }
+    return response.json();
+
+  })
+    .catch((error) => {
+      throw error;
+    });
+};
+
+export const getMovieReviews = (id) => {
+  return fetch(
+    baseUrl + `movie/${id}/reviews?api_key=${process.env.REACT_APP_TMDB_KEY}`
+  )
+    .then((res) => res.json())
+    .then((json) => {
+      // console.log(json.results);
+      return json.results;
+    });
+};
+
+export const getUpcoming = () => {
+  return fetch(
+    baseUrl + `movie/upcoming?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1`
+  ).then((response) => {
+    if (!response.ok) {
+      throw new Error(response.json().message);
+    }
+    return response.json();
+  })
+    .catch((error) => {
+      throw error;
+    });
+};
