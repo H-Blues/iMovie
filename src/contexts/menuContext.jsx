4f4238e476aa90239f8b3677faf47ca9588bdd88
Zihan Zhang <hblues419@gmail.com>
2022-11-01 17:11:44 +0000

    feat: add react-context to pass data of menu

---
+++ src/contexts/menuContext.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const list = [
+  {
+    Movies: ['Popular', 'Upcoming', 'Top Rated'],
+    TV_Shows: ['Popular'],
+    People: ['Popular People'],
+  },
+];
+
+const path = [
+  {
+    Movies: ['/movies', '/movies/upcoming', '/movies/top-rated'],
+    TV_Shows: ['/tv'],
+    People: ['/people'],
+  },
+];
+
+export const MenuContext = React.createContext({ list: list, path: path });
+
+const MenuContextProvider = (props) => {
+  return (
+    <MenuContext.Provider value={{ list: list, path: path }}>
+      {props.children}
+    </MenuContext.Provider>
+  );
+};
+export default MenuContextProvider;
